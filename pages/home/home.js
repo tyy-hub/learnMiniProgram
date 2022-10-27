9774bc0e8b4a37bc87640d92b316c7e6fef07184
tyy <1469109404@qq.com>
2022-10-27 15:59:04 +0800

    初始化

---
+++ pages/home/home.js
@@ -0,0 +1,21 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    value: 'hello world'
+  },
+
+  change () {
+    console.log(3)
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  }
+})
